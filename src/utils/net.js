7c96bda33e72acc03c8c8c829141df63ccbf8a2a
Vant <forvant@qq.com>
2020-04-15 00:23:07 +0800

    解决了axios发送请求不发送cookie问题，完全实现了前后端联调

--- src/utils/net.js
+++ src/utils/net.js
@@ -2,11 +2,12 @@ import Axios from "axios";
 import _ from 'lodash';
 
 const axios = Axios.create({
-    baseURL: 'http://localhost:8082/api',
+    baseURL: 'http://' + process.env.VUE_APP_BACKEND_URL + ':' + process.env.VUE_APP_BACKEND_PORT + '/api',
     timeout: 300 * 1000,
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
     },
+    withCredentials: true,
     transformRequest: data => {
         let f = new FormData();
         let array = Object.keys(_.defaultTo(data, {}));
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // 开发代理服务器
+    // devServer: {
+    //     proxy: {
+    //         '/api': {
+    //             target: 'http://' + process.env.VUE_APP_BACKEND_URL + ':' + process.env.VUE_APP_BACKEND_PORT + '/api',
+    //             changeOrigin: true,
+    //             ws: true
+    //         }
+    //     }
+    // },
+}
